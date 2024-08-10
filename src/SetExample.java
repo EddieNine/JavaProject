af87003fdd64ffe135e33867735c59f4a282a1c0
EddieNine <believefixed@hotmail.com>
2024-08-10 14:36:50 -0300

    Operações com Conjuntos

--- src/SetExample.java
+++ src/SetExample.java
@@ -4,7 +4,7 @@ import java.util.Set;
 public class SetExample {
     public static void main(String[] args) {
         Set<String> contacts = new HashSet<>();
-        
+
         contacts.add("Alice");
         contacts.add("Bob");
         contacts.add("Alice"); // Não será adicionado novamente
