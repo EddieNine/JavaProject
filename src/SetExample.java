b2426d8688d71d5fd21cb2027113d5655871c6ee
EddieNine <believefixed@hotmail.com>
2024-08-10 14:31:24 -0300

    Introdução a Conjuntos

---
+++ src/SetExample.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetExample {
+    public static void main(String[] args) {
+        Set<String> contacts = new HashSet<>();
+        
+        contacts.add("Alice");
+        contacts.add("Bob");
+        contacts.add("Alice"); // Não será adicionado novamente
+
+        System.out.println("Contacts: " + contacts);
+    }
+}
